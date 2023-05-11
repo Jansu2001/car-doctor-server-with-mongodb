6f5cdb40448935087f412f52fee811a8fca28825
jansu2001 <sadnansujan23@gmail.com>
2023-05-12 00:21:09 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -33,7 +33,71 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const carCollection = client.db('carDoctor').collection('services')
+        const bookingsCollection = client.db('carDoctor').collection('bookings')
+
+
+        app.get('/services', async (req, res) => {
+            const cursor = carCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const options = {
+                projection: { title: 1, price: 1, img: 1, date: 1 },
+            };
+            const result = await carCollection.findOne(query, options)
+            res.send(result)
+        })
+
+        //Bookings Collection
+
         
+        app.delete('/bookings/:id', async (req, res)=>{
+            const id =req.params.id;
+            const query={_id: new ObjectId(id)}
+            const result = await bookingsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        // Change Please-Confirm TO Confirm
+        app.patch('/bookings/:id', async (req,res)=>{
+            const id=req.params.id
+            const query={_id: new ObjectId(id)}
+            const updatedBooking=req.body;
+            const updatedDoc={
+                $set:{
+                    status:updatedBooking.status
+                }
+            }
+            const result= await bookingsCollection.updateOne(query,updatedDoc)
+            res.send(result)
+        })
+
+
+        app.get('/bookings', async (req, res) => {
+            console.log((req.query.email));
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking)
+            res.send(result)
+
+        })
 
 
 
